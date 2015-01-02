0da916a04431e0d93bef5e9b5144dd6f58beb865
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-01-02 06:28:41 +0100

    PermissionCreate view created

--- poradnia/cases/views.py
+++ poradnia/cases/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView, UpdateView, DeleteView, ListView
+from django.views.generic import DetailView, UpdateView, CreateView, DeleteView, ListView
 from django.http import Http404
 from braces.views import FormValidMessageMixin
 from .models import Case, Permission
@@ -12,10 +12,29 @@ class CaseDetail(PermissionGroupMixin, DetailView):
 class CaseList(PermissionGroupMixin, ListView):
     model = Case
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(CaseList, self).get_queryset(*args, **kwargs)
+        return queryset.without_lawyers()
+
 
 class CaseEdit(FormValidMessageMixin, UpdateView, PermissionGroupMixin):
     model = Case
     fields = ("name", "status")
+
+    def get_form_valid_message(self):
+        return u"{0} updated!".format(self.object)
+
+
+class PermissionCreate(FormValidMessageMixin, PermissionGroupMixin, CreateView):
+    model = Permission
+    fields = ('user', 'rank')
+
+    def form_valid(self, form):
+        form.instance.case_id = self.kwargs['case_id']
+        return super(PermissionCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        return self.object.case.get_absolute_url()
 
     def get_form_valid_message(self):
         return u"{0} updated!".format(self.object)
