9b4f0b132052ffe2c64367b1ad371d1e9151f07c
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-01-02 05:30:18 +0100

    Fix non-DRY code in users models using PassThroughtManager

--- poradnia/users/models.py
+++ poradnia/users/models.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
 # Import the AbstractUser model
 from django.contrib.auth.models import AbstractUser
-
-# Import the basic Django ORM models library
 from django.db import models
-
 from django.utils.translation import ugettext_lazy as _
-
 from django.db.models import Q
-
 from django.core.urlresolvers import reverse
-
 from django.db.models.query import QuerySet
-
 from django.db.models import Count
+from model_utils.managers import PassThroughManager
 
 
 class UserQuerySet(QuerySet):
@@ -26,19 +20,8 @@ class UserQuerySet(QuerySet):
         return self.annotate(case_count=Count('case'))
 
 
-class UserManager(models.Manager):  # TODO: How to write more DRY?
-    def get_queryset(self):
-        return UserQuerySet(self.model)
-
-    def for_user(self, user):
-        return self.get_query_set().for_user(user)
-
-    def with_case_count(self):
-        return self.get_query_set().with_case_count()
-
-
 class User(AbstractUser):
-    objects = UserManager()
+    objects = PassThroughManager.for_queryset_class(UserQuerySet)()
 
     def __unicode__(self):
         return self.username
