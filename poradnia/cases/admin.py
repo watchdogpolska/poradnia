e9afa8dc91b544d6843ecbf7c3f55d79191fd01a
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-01-02 05:30:54 +0100

    Added admin for SiteGroup

--- poradnia/cases/admin.py
+++ poradnia/cases/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Case, Permission
+from .models import Case, Permission, SiteGroup
 
 
 @admin.register(Case)
@@ -10,3 +10,8 @@ class CaseAdmin(admin.ModelAdmin):
 @admin.register(Permission)
 class PermissionAdmin(admin.ModelAdmin):
     list_display = ['case', 'rank']
+
+
+@admin.register(SiteGroup)
+class SiteGroupAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'rank', 'site', 'group']
