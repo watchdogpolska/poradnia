ef7f2835d646f3b79db106a7ad82d92f29f7d6d7
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-01-02 05:33:00 +0100

    Added way to access children for records

--- poradnia/records/models.py
+++ poradnia/records/models.py
@@ -10,3 +10,16 @@ class Record(models.Model):
 
     def get_absolute_url(self):
         return "%s#%d" % (self.case.get_absolute_url(), self.pk)
+
+    def get_child_name(self):
+        fields = ('letter', 'event', 'alarm', 'following')  # How to write it better?
+        for field in fields:
+            if hasattr(self, field):
+                return field
+        return None
+
+    def get_child_obj(self):
+        return getattr(self, self.get_child_name())
+
+    def get_template_list(self):
+        return "%s/_%s_list.html" % (self._meta.app_label, self._meta.model_name)
