001bbf0ef11ad42ba4fa04c9501d707f93b506b2
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-01-02 01:04:31 +0100

    Added admin for tracking.models.Following

--- poradnia/tracking/admin.py
+++ poradnia/tracking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Following
 
-# Register your models here.
+
+class FollowingAdmin(admin.ModelAdmin):
+    list_display = ['user', 'rank', 'case']
+admin.site.register(Following, FollowingAdmin)
