2864f92f90b9e40403c8f006829fc285fa3650a1
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-05-07 11:55:29 +0200

    Drop SendGrid, AWS; disable HSTS

--- poradnia/config/production.py
+++ poradnia/config/production.py
@@ -38,14 +38,14 @@ class Production(Common):
     INSTALLED_APPS += ("djangosecure", )
 
     # set this to 60 seconds and then to 518400 when you can prove it works
-    SECURE_HSTS_SECONDS = 60
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
-    SECURE_FRAME_DENY = values.BooleanValue(True)
-    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
-    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
-    SESSION_COOKIE_SECURE = values.BooleanValue(False)
-    SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
-    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    # SECURE_HSTS_SECONDS = 60
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    # SECURE_FRAME_DENY = values.BooleanValue(True)
+    # SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    # SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    # SESSION_COOKIE_SECURE = values.BooleanValue(False)
+    # SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
+    # SECURE_SSL_REDIRECT = values.BooleanValue(True)
     # end django-secure
 
     # SITE CONFIGURATION
@@ -56,44 +56,13 @@ class Production(Common):
 
     INSTALLED_APPS += ("gunicorn", )
 
-    # STORAGE CONFIGURATION
-    # See: http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += (
-        'storages',
-    )
-
-    # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-    STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-    # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-    AWS_ACCESS_KEY_ID = values.SecretValue()
-    AWS_SECRET_ACCESS_KEY = values.SecretValue()
-    AWS_STORAGE_BUCKET_NAME = values.SecretValue()
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_QUERYSTRING_AUTH = False
-
-    # see: https://github.com/antonagestam/collectfast
-    AWS_PRELOAD_METADATA = True
-    INSTALLED_APPS += ("collectfast", )
-
-    # AWS cache settings, don't change unless you know what you're doing:
-    AWS_EXPIREY = 60 * 60 * 24 * 7
-    AWS_HEADERS = {
-        'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (
-            AWS_EXPIREY, AWS_EXPIREY)
-    }
-
-    # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-    STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-    # END STORAGE CONFIGURATION
-
     # EMAIL
     DEFAULT_FROM_EMAIL = values.Value('poradnia <noreply@poradnia.informacjapubliczna.org.pl>')
     EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[poradnia] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    EMAIL_HOST_PASSWORD = values.SecretValue()
+    EMAIL_HOST_USER = values.SecretValue()
+    EMAIL_PORT = values.IntegerValue(587)
+    EMAIL_SUBJECT_PREFIX = values.Value('[poradnia] ')
     EMAIL_USE_TLS = True
     SERVER_EMAIL = EMAIL_HOST_USER
     # END EMAIL
