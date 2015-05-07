17ecd8f586af62030d1770f62a0a15870703a91c
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-05-07 14:52:53 +0200

    Added RecordInline to CaseAdmin

--- poradnia/cases/admin.py
+++ poradnia/cases/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 from .models import Case
+from records.models import Record
+
+
+class RecordInline(admin.StackedInline):
+    '''
+        Stacked Inline View for Record
+    '''
+    model = Record
+    min_num = 3
+    max_num = 20
+    extra = 1
 
 
 @admin.register(Case)
 class CaseAdmin(GuardedModelAdmin):
+    inlines = [RecordInline]
     list_display = ['name', 'client']
