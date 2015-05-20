7734a1fc149cfde1dc68dd1b357e0f25f0985cf0
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-05-21 01:04:52 +0200

    Added verbose_name in migration

---
+++ poradnia/registers/migrations/0007_auto_20150520_1509.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('registers', '0006_auto_20150514_0226'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='advice',
+            name='case',
+            field=models.OneToOneField(null=True, blank=True, to='cases.Case', verbose_name='Case'),
+            preserve_default=True,
+        ),
+    ]
