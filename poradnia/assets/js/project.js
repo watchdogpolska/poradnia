867d9ccd9a69fc192ad518713a99ff15c7aa9d39
Dzakus <Dzakus13@gmail.com>
2015-07-12 13:40:12 +0200

    Enable script for tooltip

--- poradnia/assets/js/project.js
+++ poradnia/assets/js/project.js
@@ -1,2 +1,7 @@
 /* Project specific Javascript goes here. */
  $('input.datetimeinput').datetimepicker({ format: 'YYYY-MM-DD HH:mm' ,sideBySide: true});
+
+
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+})
