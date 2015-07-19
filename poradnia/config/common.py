300c5838898be2a7a9e8cb8e8c3e6a159456a879
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-07-19 14:54:09 +0200

    Email verification is optional

--- poradnia/config/common.py
+++ poradnia/config/common.py
@@ -235,7 +235,7 @@ class Common(Configuration):
     # Some really nice defaults
     ACCOUNT_AUTHENTICATION_METHOD = "username"
     ACCOUNT_EMAIL_REQUIRED = True
-    ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+    ACCOUNT_EMAIL_VERIFICATION = "optional"
     # END AUTHENTICATION CONFIGURATION
 
     # Custom user app defaults
