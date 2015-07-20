c423eb5c95434024a67669839db794a35b56a771
Dzakus <Dzakus13@gmail.com>
2015-07-21 01:28:51 +0200

    Add support for datapicker on awesome fields

--- poradnia/static/js/script.js
+++ poradnia/static/js/script.js
@@ -14441,17 +14441,20 @@ return jQuery;
 })(jQuery);
 
 $(function(){
-	var timepicker = new Pikaday(
-	{
-		field: document.querySelector('#id_time'),
-		firstDay: 1,
-		format: 'DD.MM.YYYY HH:mm:ss',
-		minDate: new Date('2000-01-01'),
-		maxDate: new Date('2020-12-31'),
-		yearRange: [2000,2020],
-		showTime: true,
-		use24hour: true
-	});
+	var fields = document.querySelectorAll('.datetimeinput');
+	var fields_array = Array.prototype.slice.call(fields);
+	fields_array.forEach(function(item){
+		new Pikaday({
+			field: item,
+			firstDay: 1,
+			format: 'DD.MM.YYYY HH:mm:ss',
+			minDate: new Date('2000-01-01'),
+			maxDate: new Date('2020-12-31'),
+			yearRange: [2000,2020],
+			showTime: true,
+			use24hour: true
+		});
+	})
 });
 
 $(function () {
