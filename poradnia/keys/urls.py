db8c9f5371a76ac7859e8e2f2e4d657cacf12612
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-07-25 05:38:44 +0200

    Translate URLs

--- poradnia/keys/urls.py
+++ poradnia/keys/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^/$', views.KeyListView.as_view(), name="list"),
-    url(r'^/(?P<pk>\d+)$', views.KeyDetailView.as_view(), name="details"),
-    url(r'^/(?P<pk>\d+)/delete$', views.KeyDeleteView.as_view(), name="delete"),
-    url(r'^/create$', views.KeyCreateView.as_view(), name="create"),
+    url(r'^/(?P<pk>\d+)/$', views.KeyDetailView.as_view(), name="details"),
+    url(r'^/(?P<pk>\d+)/usun/$', views.KeyDeleteView.as_view(), name="delete"),
+    url(r'^/stworz/$', views.KeyCreateView.as_view(), name="create"),
 )
