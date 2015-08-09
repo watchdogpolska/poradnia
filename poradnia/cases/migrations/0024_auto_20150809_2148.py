5c210bf222a5df43077c887770f053707161552a
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-08-09 23:38:36 +0200

    Fix status in case

--- poradnia/cases/migrations/0024_auto_20150809_2148.py
+++ poradnia/cases/migrations/0024_auto_20150809_2148.py
@@ -9,13 +9,14 @@ def delete_empty(apps, schema_editor):
     # We get the model from the versioned app registry;
     # if we directly import it, it'll be the wrong version
     Case = apps.get_model("cases", "Case")
-    Case.objects.annotate(record_count=Count('record')).filter(record_count=0).update(status=Case.STATUS.closed)
+    Case.objects.annotate(record_count=Count('record')).filter(record_count=0).update(status='2')
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('cases', '0023_auto_20150809_2131'),
+        ('records', '0006_auto_20150503_1741'),
     ]
 
     operations = [
