997c07be0f0437d907fd78a07244b265e76f2158
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-08-19 20:29:16 +0200

    Drop system locale in calendar - use django translation every time

--- poradnia/events/utils.py
+++ poradnia/events/utils.py
@@ -1,15 +1,43 @@
-from calendar import LocaleHTMLCalendar
+from calendar import HTMLCalendar
 from datetime import date
 from itertools import groupby
 from django.utils.html import conditional_escape as esc
+from django.utils.translation import ugettext_lazy as _
 
 
-class AbstractCalendar(LocaleHTMLCalendar):
+day_name = [_('Monday'), _('Tuesday'), _('Wednesday'), _('Thursday'), _('Friday'),
+    _('Saturday'), _('Sunday')]
+day_abbr = [_('Mon'), _('Tue'), _('Wed'), _('Thu'), _('Fri'), _('Sat'), _('Sun')]
+
+month_name = [_('January'), _('February'), _('March'), _('April'), _('May'), _('June'),
+    _('July'), _('August'), _('September'), _('October'), _('November'), _('December')]
+
+month_abbr = [_('Jan'), _('Feb'), _('Mar'), _('Apr'), _('May'), _('Jun'), _('Jul'),
+    _('Aug'), _('Sep'), _('Oct'), _('Nov'), _('Dec')]
+
+
+class AbstractCalendar(HTMLCalendar):
     field = 'time'
 
     def __init__(self, events, *args, **kwargs):
         super(AbstractCalendar, self).__init__(*args, **kwargs)
         self.events = self.group_by_day(events)
+
+    def formatmonthname(self, theyear, themonth, withyear=True):
+        """
+        Return a month name as a table row.
+        """
+        if withyear:
+            s = '%s %s' % (month_name[themonth], theyear)
+        else:
+            s = '%s' % month_name[themonth]
+        return '<tr><th colspan="7" class="month">%s</th></tr>' % s
+
+    def formatweekday(self, day):
+        """
+        Return a weekday name as a table header.
+        """
+        return '<th class="%s">%s</th>' % (self.cssclasses[day], day_abbr[day])
 
     def formatday(self, day, weekday):
         if day != 0:
@@ -32,13 +60,16 @@ class AbstractCalendar(LocaleHTMLCalendar):
         return super(AbstractCalendar, self).formatmonth(year, month)
 
     def group_by_day(self, events):
-        return {day: list(items) for day, items in groupby(events, lambda x: getattr(x, self.field).day)}
+        return {day: list(items) for day, items in groupby(events, self.get_field)}
 
     def day_cell(self, cssclass, body):
         return '<td class="%s">%s</td>' % (cssclass, body)
 
-    def get_field(self):
+    def get_field_name(self):
         return self.field
+
+    def get_field(self, obj):
+        return getattr(obj, self.get_field_name()).day
 
     def get_row_content(self, event):
         raise NotImplementedError("Method 'get_row_content' should be overwriten.")
