b32dd0b1a0f836d5dc57c1aa8683619130f48d59
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-09-09 03:09:12 +0200

    Add filter by no reply - close #148

--- poradnia/cases/filters.py
+++ poradnia/cases/filters.py
@@ -5,6 +5,16 @@ from users.filters import UserChoiceFilter
 from .models import Case
 
 
+class NullDateRangeFilter(django_filters.DateRangeFilter):
+    def __init__(self, none_label=None, *args, **kwargs):
+        if not none_label:
+            none_label = _('None')
+        self.options[6] = (none_label,
+                           lambda qs, name: qs.filter(**{"%s__isnull" % name: True})
+                           )
+        super(NullDateRangeFilter, self).__init__(*args, **kwargs)
+
+
 class CaseFilterMixin(object):
     def __init__(self, *args, **kwargs):
         super(CaseFilterMixin, self).__init__(*args, **kwargs)
@@ -16,7 +26,7 @@ class StaffCaseFilter(CrispyFilterMixin, CaseFilterMixin, django_filters.FilterS
     client = UserChoiceFilter(label=_("Client"))
     created_by = UserChoiceFilter(label=_("Created by"))
     created_on = django_filters.DateRangeFilter(label=_("Created on"))
-    last_send = django_filters.DateRangeFilter(label=_("Last send"))
+    last_send = NullDateRangeFilter(label=_("Last send"), none_label=_("No reply"))
     last_action = django_filters.DateRangeFilter(label=_("Last action"))
 
     def get_order_by(self, order_choice):
