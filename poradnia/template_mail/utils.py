13ee6149ff7eb0ea216d1408295860613ce4d290
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-09-11 14:43:55 +0200

    Fix subject in template_mail

--- poradnia/template_mail/utils.py
+++ poradnia/template_mail/utils.py
@@ -8,7 +8,7 @@ def send_tpl_email(template_name, recipient_list, context=None, from_email=None,
     c = Context(context or {})
     subject, txt = t.render(c).split("\n", 1)
     from_email = from_email if from_email else settings.DEFAULT_FROM_EMAIL
-    send_mail(subject=subject.split(),
+    send_mail(subject=subject.strip(),
               message=txt,
               from_email=from_email,
               recipient_list=recipient_list,
