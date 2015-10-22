14e0bf1c51b83c17aa701a958d5cdd0374730ac9
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-23 00:53:18 +0200

    Definitly drop django-avatar

--- poradnia/config/common.py
+++ poradnia/config/common.py
@@ -38,7 +38,6 @@ class Common(Configuration):
     )
     THIRD_PARTY_APPS = (
         'crispy_forms',  # Form layouts
-        'avatar',  # for user avatars
         'allauth',  # registration
         'allauth.account',  # registration
         'allauth.socialaccount',  # registration
