3ae37e7811ab27ed7a1611809d173ea7f91d0978
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-23 07:25:37 +0200

    Fix django-allauth context processors again

--- poradnia/config/common.py
+++ poradnia/config/common.py
@@ -168,7 +168,6 @@ class Common(Configuration):
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
     TEMPLATE_CONTEXT_PROCESSORS = (
         'django.contrib.auth.context_processors.auth',
-        "allauth.socialaccount.context_processors.socialaccount",
         'django.core.context_processors.debug',
         'django.core.context_processors.i18n',
         'django.core.context_processors.media',
