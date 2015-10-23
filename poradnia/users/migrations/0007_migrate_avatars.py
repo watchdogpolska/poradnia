ad6d981cfbb9af0b02b40346548eb37631538016
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-23 06:53:11 +0200

    Fix migrations after django-avatar drop

--- poradnia/users/migrations/0007_migrate_avatars.py
+++ poradnia/users/migrations/0007_migrate_avatars.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models, migrations
 
 
-def migrate_avatar(apps, schema_editor):
-    Avatar = apps.get_model("avatar", "Avatar")
-    for avatar in Avatar.objects.filter(primary=True).all():
-        avatar.user.picture = avatar.avatar
-        avatar.user.save()
-        avatar.save()
+if 'avatar' in settings.INSTALLED_APPS:
+    def migrate_avatar(apps, schema_editor):
+        Avatar = apps.get_model("avatar", "Avatar")
+        for avatar in Avatar.objects.filter(primary=True).all():
+            avatar.user.picture = avatar.avatar
+            avatar.user.save()
+            avatar.save()
 
+    class Migration(migrations.Migration):
+        dependencies = [
+            ('users', '0006_user_picture'),
+            ('avatar', '0001_initial')
+        ]
 
-class Migration(migrations.Migration):
-    dependencies = [
-        ('users', '0006_user_picture'),
-        ('avatar', '0001_initial')
-    ]
+        operations = [
+            migrations.RunPython(migrate_avatar)
 
-    operations = [
-        migrations.RunPython(migrate_avatar)
-
-    ]
+        ]
+else:
+    class Migration(migrations.Migration):
+        dependencies = [('users', '0006_user_picture'), ]
+        operations = []
