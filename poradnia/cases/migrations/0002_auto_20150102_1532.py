f053615c51a7b937e4dedc561757f675e95380a7
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-12-24 23:55:34 +0100

    Fix cases migrations after drop tags

--- poradnia/cases/migrations/0002_auto_20150102_1532.py
+++ poradnia/cases/migrations/0002_auto_20150102_1532.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     dependencies = [
         ('cases', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('tags', '0001_initial'),
     ]
 
     operations = [
@@ -18,12 +17,6 @@ class Migration(migrations.Migration):
             model_name='case',
             name='client',
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='case',
-            name='tags',
-            field=models.ManyToManyField(to='tags.Tag', null=True, blank=True),
             preserve_default=True,
         ),
     ]
