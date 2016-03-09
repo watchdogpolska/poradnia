8782e9c58d3569391b7a006c67403344122ef570
rwa kulszowa <zajactomasz1@yahoo.com>
2016-03-09 18:12:05 +0100

    Implemented query for created cases

--- poradnia/stats/views.py
+++ poradnia/stats/views.py
@@ -1,4 +1,25 @@
+import json
+
+from django.db.models import F, Func, Count, IntegerField
 from django.http import JsonResponse
 
+from cases.models import Case
+
 def case_stats_view(request):
-    return JsonResponse("test", safe=False)
+    qs = (
+        Case.objects.annotate(
+            month=Func(F('created_on'),
+            function='month',
+            output_field=IntegerField())
+        ).annotate(
+            year=Func(F('created_on'),
+            function='year',
+            output_field=IntegerField())
+        ).values('month','year')
+        .annotate(
+            count=Count(F('id'))
+        ).values('month','year','count')
+        .order_by(F('year'), F('month'))
+    )
+
+    return JsonResponse(json.dumps(list(qs)), safe=False)
