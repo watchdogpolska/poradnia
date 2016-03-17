658d46e3de95f7f14b4ea517a19063141622d22b
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-03-17 23:58:14 +0100

    PEP8 fix

--- poradnia/stats/views.py
+++ poradnia/stats/views.py
@@ -1,6 +1,4 @@
-import json
-
-from django.db.models import F, Func, Count, IntegerField, Case, Sum, When
+from django.db.models import F, Func, IntegerField, Case, Sum, When
 from django.http import JsonResponse
 from django.views.generic import TemplateView
 
@@ -10,14 +8,14 @@ from cases.models import Case as CaseModel
 def case_stats_api_view(request):
     qs = (
         CaseModel.objects.annotate(
-            month=Func(F('created_on'),
-            function='month',
-            output_field=IntegerField())
+                                    month=Func(F('created_on'),
+                                               function='month',
+                                               output_field=IntegerField())
         ).annotate(
-            year=Func(F('created_on'),
-            function='year',
-            output_field=IntegerField())
-        ).values('month','year')
+                    year=Func(F('created_on'),
+                              function='year',
+                              output_field=IntegerField())
+        ).values('month', 'year')
         .annotate(
             count_open=Sum(
                 Case(
@@ -49,8 +47,3 @@ def case_stats_api_view(request):
 
 class StatsCaseView(TemplateView):
     template_name = 'stats/cases.html'
-    data_view = case_stats_api_view
-
-    def get_context_data(self, **kwargs):
-        context = super(StatsCaseView, self).get_context_data(**kwargs)
-        return context
