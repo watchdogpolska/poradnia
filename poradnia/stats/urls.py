c6cfa2c39f2af0db5b7e010cd36b0828c20bc500
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-03-18 00:20:20 +0100

    Add StatsCaseApiView, ApiListViewMixin

--- poradnia/stats/urls.py
+++ poradnia/stats/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^api/sprawy/$', views.case_stats_api_view, name="case_stats_api"),
+    url(r'^api/sprawy/$', views.StatsCaseApiView.as_view(), name="case_stats_api"),
     url(r'^sprawy/$', views.StatsCaseView.as_view(), name="case_stats")
 ]
--- poradnia/stats/views.py
+++ poradnia/stats/views.py
@@ -1,49 +1,53 @@
 from django.db.models import F, Func, IntegerField, Case, Sum, When
-from django.http import JsonResponse
+from braces.views import JSONResponseMixin
 from django.views.generic import TemplateView
-
+from django.views.generic import View
 from cases.models import Case as CaseModel
 
 
-def case_stats_api_view(request):
-    qs = (
-        CaseModel.objects.annotate(
-                                    month=Func(F('created_on'),
-                                               function='month',
-                                               output_field=IntegerField())
-        ).annotate(
-                    year=Func(F('created_on'),
-                              function='year',
-                              output_field=IntegerField())
-        ).values('month', 'year')
-        .annotate(
-            count_open=Sum(
-                Case(
-                    When(status=CaseModel.STATUS.free, then=1),
-                    default=0,
-                    output_field=IntegerField()
-                )
-            ),
-            count_assigned=Sum(
-                Case(
-                    When(status=CaseModel.STATUS.assigned, then=1),
-                    default=0,
-                    output_field=IntegerField()
-                )
-            ),
-            count_closed=Sum(
-                Case(
-                    When(status=CaseModel.STATUS.closed, then=1),
-                    default=0,
-                    output_field=IntegerField()
-                )
-            )
-        ).values('month', 'year', 'count_open', 'count_assigned', 'count_closed')
-        .order_by(F('year'), F('month'))
-    )
-
-    return JsonResponse(list(qs), safe=False)
+class ApiListViewMixin(JSONResponseMixin):
+    def get(self, request, *args, **kwargs):
+        return self.render_json_response(list(self.get_object_list()))
 
 
 class StatsCaseView(TemplateView):
     template_name = 'stats/cases.html'
+
+
+class StatsCaseApiView(ApiListViewMixin, View):
+    def get_object_list(self):
+        return (
+            CaseModel.objects.annotate(
+                                        month=Func(F('created_on'),
+                                                   function='month',
+                                                   output_field=IntegerField())
+            ).annotate(
+                        year=Func(F('created_on'),
+                                  function='year',
+                                  output_field=IntegerField())
+            ).values('month', 'year')
+            .annotate(
+                count_open=Sum(
+                    Case(
+                        When(status=CaseModel.STATUS.free, then=1),
+                        default=0,
+                        output_field=IntegerField()
+                    )
+                ),
+                count_assigned=Sum(
+                    Case(
+                        When(status=CaseModel.STATUS.assigned, then=1),
+                        default=0,
+                        output_field=IntegerField()
+                    )
+                ),
+                count_closed=Sum(
+                    Case(
+                        When(status=CaseModel.STATUS.closed, then=1),
+                        default=0,
+                        output_field=IntegerField()
+                    )
+                )
+            ).values('month', 'year', 'count_open', 'count_assigned', 'count_closed')
+            .order_by(F('year'), F('month'))
+        )
