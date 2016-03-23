f08d5d9f4fb04de0af2fdcf5281622b55af31933
rwa kulszowa <zajactomasz1@yahoo.com>
2016-03-23 19:33:09 +0100

    Added initial case reaction render template

--- poradnia/stats/urls.py
+++ poradnia/stats/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.StatsIndexView.as_view(), name="index"),
-    url(r'^api/sprawy/czas_reakcji$', views.StatsCaseReactionApiView.as_view(), name="case_reaction_stats_api"),
+    url(r'^api/sprawy/czas_reakcji$', views.StatsCaseReactionApiView.as_view(), name="case_stats_reaction_api"),
     url(r'^api/sprawy/utworzone$', views.StatsCaseCreatedApiView.as_view(), name="case_stats_created_api"),
     url(r'^render/sprawy/utworzone$', views.StatsCaseCreatedRenderView.as_view(), name="case_stats_created_render"),
+    url(r'^render/sprawy/czas_reakcji$', views.StatsCaseReactionRenderView.as_view(), name="case_stats_reaction_render"),
     url(r'^sprawy/utworzone$', views.StatsCaseCreatedView.as_view(), name="case_stats_created"),
 ]
