897a5d76c14b20c9321a060de277061604b63512
rwakulszowa <zajactomasz1@yahoo.com>
2016-04-11 23:04:22 +0200

    Moved legend adding to a separate function

--- poradnia/stats/static/stats/chart_reaction.js
+++ poradnia/stats/static/stats/chart_reaction.js
@@ -38,6 +38,39 @@ var chart = d3.select(".chart")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+function legendWrapper(domain, config) {
+
+  var domain = domain,
+      config = config;
+
+  function legend(vis) {
+
+      var legend = vis.selectAll('.legend')
+          .data(domain)
+        .enter()
+          .append('g')
+          .attr('class', 'legend')
+          .attr('transform', function(d, i) {
+            var height = config.legendRectSize + config.legendSpacing;
+            var horz = config.legendRectSize;
+            var vert = i * height;
+            return 'translate(' + horz + ',' + vert + ')';
+          });
+
+      legend.append('rect')
+          .attr('width', config.legendRectSize)
+          .attr('height', config.legendRectSize)
+          .style('fill', (function(d) { return color(d); }));
+
+      legend.append('text')
+          .attr('x', config.legendSpacing + config.legendRectSize)
+          .attr('y', config.legendSpacing + config.legendRectSize / 2)
+          .text(function(d) { return d; });
+      }
+
+  return legend;
+}
+
 function reaction_chart(error, data) {
   if (error) throw error;
 
@@ -65,27 +98,7 @@ function reaction_chart(error, data) {
       .attr("d", area)
       .style("fill", color("time_delta"));
 
+  var legend = legendWrapper(color.domain(), {'legendSpacing': legendSpacing, 'legendRectSize': legendRectSize});
 
-  var legend = chart.selectAll('.legend')
-      .data(color.domain())
-    .enter()
-      .append('g')
-      .attr('class', 'legend')
-      .attr('transform', function(d, i) {
-        var height = legendRectSize + legendSpacing;
-        var horz = legendRectSize;
-        var vert = i * height;
-        return 'translate(' + horz + ',' + vert + ')';
-      });
-
-  legend.append('rect')
-      .attr('width', legendRectSize)
-      .attr('height', legendRectSize)
-      .style('fill', color)
-      .style('stroke', color);
-
-  legend.append('text')
-      .attr('x', legendSpacing + legendRectSize)
-      .attr('y', legendSpacing + legendRectSize / 2)
-      .text(function(d) { return d; });
+  chart.call(legend);
 }
