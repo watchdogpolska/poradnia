7c6cfb5a224e50cb6becfca95d776d4924e3f5f4
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-04-13 05:36:39 +0200

    Fix case migrations for SQLite

--- poradnia/cases/migrations/0024_auto_20150809_2148.py
+++ poradnia/cases/migrations/0024_auto_20150809_2148.py
@@ -9,7 +9,8 @@ def delete_empty(apps, schema_editor):
     # We get the model from the versioned app registry;
     # if we directly import it, it'll be the wrong version
     Case = apps.get_model("cases", "Case")
-    Case.objects.annotate(record_count=Count('record')).filter(record_count=0).update(status='2')
+    pks = Case.objects.annotate(record_count=Count('record')).filter(record_count=0).values('id')
+    Case.objects.filter(pk__in=pks).update(status='2')
 
 
 class Migration(migrations.Migration):
