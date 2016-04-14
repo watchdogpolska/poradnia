eb0ef42918aca33d5876840c1d3df91c8c4dfe47
rwakulszowa <zajactomasz1@yahoo.com>
2016-04-14 21:13:57 +0200

    Moved chart setup to common.js

--- poradnia/stats/static/stats/chart_reaction.js
+++ poradnia/stats/static/stats/chart_reaction.js
@@ -1,28 +1,3 @@
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var legendRectSize = 16,
-    legendSpacing = 4;
-
-var x = d3.time.scale()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var color = d3.scale.category20c();
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var parseDate = d3.time.format("%Y-%m-%d").parse;
-
 var area = d3.svg.area()
     .interpolate("monotone")
     .x(function(d) { return x(d.date); })
---
+++ poradnia/stats/static/stats/common.js
@@ -0,0 +1,24 @@
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var legendRectSize = 16,
+    legendSpacing = 4;
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.category20c();
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var parseDate = d3.time.format("%Y-%m-%d").parse;
