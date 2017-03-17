9c8234ec39c38a3c6af55adeefcb23c4f184730f
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-03-17 20:31:49 +0100

    Fix location of settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -22,7 +22,7 @@ from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = ".settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
