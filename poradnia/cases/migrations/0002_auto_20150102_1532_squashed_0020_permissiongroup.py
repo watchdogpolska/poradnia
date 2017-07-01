72bd425f511992deac1ce72374ca78ac7514eb7a
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-07-01 16:31:31 +0200

    Fix python3 compatibility of cases migrations

--- poradnia/cases/migrations/0002_auto_20150102_1532_squashed_0020_permissiongroup.py
+++ poradnia/cases/migrations/0002_auto_20150102_1532_squashed_0020_permissiongroup.py
@@ -209,7 +209,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=25, verbose_name='Name')),
-                ('permissions', models.ManyToManyField(to=b'auth.Permission', verbose_name='Permissions')),
+                ('permissions', models.ManyToManyField(to='auth.Permission', verbose_name='Permissions')),
             ],
         ),
     ]
