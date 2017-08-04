6be26fabe4026ee14dbd97024fdf63367693bebe
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-08-04 22:13:57 +0200

    Update Django 1.19 compatibility

--- poradnia/stats/models.py
+++ poradnia/stats/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.db.models import QuerySet, Max
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
