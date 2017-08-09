c78989d6bc187f677db144c818ebb6aa6851ffc3
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-08-10 00:04:22 +0200

    Reduce timezone warnings

--- poradnia/stats/factories.py
+++ poradnia/stats/factories.py
@@ -1,14 +1,16 @@
 # coding=utf-8
 import factory.fuzzy
 import factory
+import pytz
 from django.utils.datetime_safe import datetime
+from pytz import timezone
 
 
 class ItemFactory(factory.django.DjangoModelFactory):
     key = factory.Sequence("item-key-{0}".format)
     name = factory.Sequence("item-name-{0}".format)
     description = factory.fuzzy.FuzzyText()
-    last_updated = factory.fuzzy.FuzzyNaiveDateTime(datetime(2008, 1, 1))
+    last_updated = factory.fuzzy.FuzzyDateTime(datetime(2008, 1, 1, tzinfo=pytz.utc))
     public = factory.Sequence(lambda n: n % 2 == 0)
 
     class Meta:
@@ -17,7 +19,7 @@ class ItemFactory(factory.django.DjangoModelFactory):
 
 class ValueFactory(factory.django.DjangoModelFactory):
     item = factory.SubFactory("poradnia.stats.tests.factories.ItemFactory")
-    time = factory.fuzzy.FuzzyNaiveDateTime(datetime(2008, 1, 1))
+    time = factory.fuzzy.FuzzyDateTime(datetime(2008, 1, 1, tzinfo=pytz.utc))
     value = factory.Sequence(lambda n: n*n)
     comment = factory.Sequence("value-comment-{0}".format)
 
