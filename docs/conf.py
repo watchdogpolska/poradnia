a7d48deab324faf19f1f7e22d45c66464067fc9f
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-09-21 23:53:38 +0200

    Update import in docs in python3

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import inspect
 import django
 from django.utils.html import strip_tags
 from django.core.urlresolvers import get_resolver
-from django.utils.encoding import force_unicode
+from django.utils.encoding import force_str
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -327,11 +327,11 @@ def process_django_model(app, what, name, obj, options, lines):
 
         for field in fields:
             # Decode and strip any html out of the field's help text
-            help_text = strip_tags(force_unicode(field.help_text))
+            help_text = strip_tags(force_str(field.help_text))
 
             # Decode and capitalize the verbose name, for use if there isn't
             # any help text
-            verbose_name = force_unicode(field.verbose_name).capitalize()
+            verbose_name = force_str(field.verbose_name).capitalize()
 
             if help_text:
                 # Add the model field to the end of the docstring as a param
