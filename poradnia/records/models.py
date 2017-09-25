531d42041e4215a8c9a72f25755a739595a10254
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-09-25 06:14:08 +0200

    Fix syntax errors in records/models.py

--- poradnia/records/models.py
+++ poradnia/records/models.py
@@ -103,7 +103,7 @@ class AbstractRecord(models.Model):
 
     def __str__(self):
         return _("%(object)s (#%(pk)d) in case #%(case_id)d".format(object=self._meta.model_name,
-                                                                    pk=self.pk)
+                                                                    pk=self.pk))
 
     class Meta:
         abstract = True
