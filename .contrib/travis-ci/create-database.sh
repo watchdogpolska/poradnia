755b33cc046e012bb4b0fd27506314f24a2b084b
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-04-23 01:34:15 +0200

    Fix typo in create-database.sh

--- .contrib/travis-ci/create-database.sh
+++ .contrib/travis-ci/create-database.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "CREATE DATABABASE test_poradnia DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;" | mysql
+echo "CREATE DATABASE test_poradnia DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;" | mysql
