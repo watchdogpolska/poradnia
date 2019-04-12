9cceec1fb8487df2a26bfedfde4e850175abbcb3
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-04-12 03:53:14 +0200

    Create production database

--- .contrib/travis-ci/create-database.sh
+++ .contrib/travis-ci/create-database.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 echo "CREATE DATABASE test_poradnia DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;" | mysql
+echo "CREATE DATABASE poradnia DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;" | mysql
