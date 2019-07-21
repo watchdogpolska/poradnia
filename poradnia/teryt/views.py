685d6c239c36a7aa000c2d924db5b05f06c0d248
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-07-21 18:22:24 +0200

    Add filtering by advicer

--- poradnia/teryt/views.py
+++ poradnia/teryt/views.py
@@ -1,10 +1,22 @@
 from django.views.generic import DetailView, ListView
 
+from poradnia.advicer.models import Advice
+from poradnia.cases.models import Case
 from .models import JST
 
 
 class JSTDetailView(DetailView):
     model = JST
+
+    def get_context_data(self, **kwargs):
+        context = super(JSTDetailView, self).get_context_data(**kwargs)
+        context['advice_qs'] = Advice.objects.for_user(
+            self.request.user
+        ).area(self.object).all()
+        context['case_qs'] = Case.objects.for_user(
+            self.request.user
+        ).area(self.object).all()
+        return context
 
 
 class JSTListView(ListView):
