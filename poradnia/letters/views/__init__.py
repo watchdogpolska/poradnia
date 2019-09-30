fc12033181c77814eea4e745b1ea0a02f92ab56d
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-09-30 04:41:58 +0200

    Fix import

--- poradnia/letters/views/__init__.py
+++ poradnia/letters/views/__init__.py
@@ -2,7 +2,7 @@ from .cbv import (
     NewCaseCreateView,
     LetterUpdateView,
     LetterListView,
-    ReceiveEmail
+    ReceiveEmailView
 )  # noqa
 from .fbv import add, send, detail  # noqa
 from .attachments import StreamAttachmentView  # noqa
