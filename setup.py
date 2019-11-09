0deaf3e108c66ae9d7b87147eee53c33b65c45fc
Adam Dobrawy <ad-m@users.noreply.github.com>
2019-11-09 13:37:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
     author_email="naczelnik@jawnosc.tk",
     packages=["poradnia"],
     include_package_data=True,
-    install_requires=["Django>=1.7.10"],
+    install_requires=["Django>=2.2.0"],
     zip_safe=False,
+    python_requires='>=3',
     scripts=["poradnia/manage.py"],
 )
