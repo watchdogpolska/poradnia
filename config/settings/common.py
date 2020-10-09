790ea438c52e8d879be61f5aa268fc9e52c8a7bb
Marcin Wicha <marcin_wicha@outlook.com>
2020-10-10 01:01:18 +0200

    Add RICH_TEXT_ENABLED to ENV variables. Format using black

--- config/settings/common.py
+++ config/settings/common.py
@@ -341,7 +341,8 @@ STAT_METRICS = {
 
 FILE_UPLOAD_PERMISSIONS = 0o644
 
-RICH_TEXT_ENABLED = True # Enable rich text formatting. Reverts to legacy plain text display when disabled.
+# Enable rich text formatting. Reverts to legacy plain text display when disabled.
+RICH_TEXT_ENABLED = env.bool("DJANGO_RICH_TEXT_ENABLED", True) 
 
 # Django-GitHub-Revision
 GITHUB_REVISION_REPO_URL = "https://github.com/watchdogpolska/poradnia"
--- poradnia/letters/templatetags/format_text.py
+++ poradnia/letters/templatetags/format_text.py
@@ -7,8 +7,38 @@ import mistune
 
 register = template.Library()
 cleaner = Cleaner(
-tags=['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'hr', 'pre', 'img'],
-attributes={'a': ['href', 'title'],'img': ['alt', 'src', 'title'] ,'abbr': ['title'], 'acronym': ['title']})
+    tags=[
+        "a",
+        "abbr",
+        "acronym",
+        "b",
+        "blockquote",
+        "code",
+        "em",
+        "i",
+        "li",
+        "ol",
+        "strong",
+        "ul",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "p",
+        "hr",
+        "pre",
+        "img",
+    ],
+    attributes={
+        "a": ["href", "title"],
+        "img": ["alt", "src", "title"],
+        "abbr": ["title"],
+        "acronym": ["title"],
+    },
+)
+
 
 @register.filter()
 @stringfilter
@@ -17,6 +47,7 @@ def format_text(text):
         return parse_markdown(text)
     else:
         return linebreaks_filter(text)
+
 
 def parse_markdown(text):
     md = mistune.create_markdown()
