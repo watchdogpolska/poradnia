f8f1b8a6a0bfcf5837cff32efe124315695f1d83
Marcin Wicha <marcin_wicha@outlook.com>
2020-10-10 00:24:05 +0200

    Add Bleach sanitizer for complete safety.

--- poradnia/letters/templatetags/format_text.py
+++ poradnia/letters/templatetags/format_text.py
@@ -2,9 +2,13 @@ from django import template
 from django.template.defaultfilters import stringfilter, linebreaks_filter
 from django.utils.safestring import mark_safe
 from django.conf import settings
+from bleach.sanitizer import Cleaner
 import mistune
 
 register = template.Library()
+cleaner = Cleaner(
+tags=['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'hr', 'pre', 'img'],
+attributes={'a': ['href', 'title'],'img': ['alt', 'src', 'title'] ,'abbr': ['title'], 'acronym': ['title']})
 
 @register.filter()
 @stringfilter
@@ -17,4 +21,5 @@ def format_text(text):
 def parse_markdown(text):
     md = mistune.create_markdown()
     html = md(text)
-    return mark_safe(html)
+    sanitized = cleaner.clean(html)
+    return mark_safe(sanitized)
