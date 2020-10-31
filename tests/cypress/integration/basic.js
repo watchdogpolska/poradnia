73caeb775c477b9fc9f0de3c5d18ea375af875ab
rwakulszowa <rwakulszowa1@gmail.com>
2020-10-31 11:23:26 +0100

    Render the app in an integration test

--- tests/cypress/integration/basic.js
+++ tests/cypress/integration/basic.js
@@ -1,6 +1,7 @@
 // The simplest tests, validating that it's possible to render the app at all.
-describe('basic math', () => {
-    it('should work', () => {
-        expect(2 + 2).to.equal(4);
+describe('landing page', () => {
+    it('should render', () => {
+        cy.visit('/');
+        cy.contains('Poradnia');
     });
 });
