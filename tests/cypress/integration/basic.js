c6be4419a30d153c92a67e94f689914dd513cb79
rwakulszowa <rwakulszowa1@gmail.com>
2020-11-07 16:39:57 +0100

    Add a .prettierrc file, format basic integration test

--- tests/cypress/integration/basic.js
+++ tests/cypress/integration/basic.js
@@ -1,7 +1,7 @@
 // The simplest tests, validating that it's possible to render the app at all.
-describe('landing page', () => {
-    it('should render', () => {
-        cy.visit('/');
-        cy.contains('Poradnia');
-    });
+describe("landing page", () => {
+  it("should render", () => {
+    cy.visit("/");
+    cy.contains("Poradnia");
+  });
 });
