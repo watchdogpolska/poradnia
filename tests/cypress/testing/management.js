c0175f8f5a920a23b372bf54e63b2d886fa4f353
rwakulszowa <rwakulszowa1@gmail.com>
2020-11-24 17:25:25 +0100

    Integration test: a function to build insert queries

--- tests/cypress/testing/management.js
+++ tests/cypress/testing/management.js
@@ -1,10 +1,22 @@
+const buildInsertQuery = (tableName, keyValuePairs) => {
+  const columns = Object.keys(keyValuePairs);
+  const values = Object.values(keyValuePairs);
+  return `
+    insert into ${tableName}
+    (${columns.join(",")})
+    values
+    (${values.join(",")})`;
+};
+
+const withExtraQuotes = (str) => `"${str}"`;
+
 const addSuperUserPrivileges = (cy) => (user) => {
   const { username } = user;
   cy.task(
     "db:query",
     `update users_user
-      set is_staff=1, is_superuser=1
-      where username="${username}"`
+     set is_staff=1, is_superuser=1
+     where username="${username}"`
   );
 };
 
@@ -14,10 +26,14 @@ const createCourt = (cy) => (court) => {
   const { id, name } = court;
   cy.task(
     "db:query",
-    `insert into judgements_court
-    (id, name, created, modified, parser_key, active)
-    values
-    (${id}, "${name}", "2010-01-01", "2010-01-01", "parser_key", 1)`
+    buildInsertQuery("judgements_court", {
+      id,
+      name: withExtraQuotes(name),
+      created: withExtraQuotes("2010-01-01"),
+      modified: withExtraQuotes("2010-01-01"),
+      parser_key: withExtraQuotes("parser_key"),
+      active: 1,
+    })
   );
 };
 
