b56bd68ea7839c4168ecde42a6afc45fe72b9fb3
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-12-30 11:57:30 +0100

    Add letter staff status field

--- poradnia/letters/models.py
+++ poradnia/letters/models.py
@@ -78,6 +78,10 @@ class Letter(AbstractRecord):
         verbose_name=_("Created by"),
         on_delete=models.CASCADE,
     )
+    created_by_staff = models.BooleanField(
+        default=False,
+        verbose_name="Created by staff member"
+    ) # redundant to preserve status after member exit
     created_on = models.DateTimeField(auto_now_add=True, verbose_name=_("Created on"))
     modified_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
