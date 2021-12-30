b202f6529a11c102ed831dc834897e3f80e3e54b
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-12-30 15:16:07 +0100

    Fix formatting

--- poradnia/events/migrations/0013_auto_20211230_1124.py
+++ poradnia/events/migrations/0013_auto_20211230_1124.py
@@ -6,13 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0012_auto_20180601_1104'),
+        ("events", "0012_auto_20180601_1104"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='event',
-            name='deadline',
-            field=models.BooleanField(default=True, help_text='A significant event, especially highlighted, for example, in the list of cases.', verbose_name='Dead-line'),
+            model_name="event",
+            name="deadline",
+            field=models.BooleanField(
+                default=True,
+                help_text="A significant event, especially highlighted, for example, in the list of cases.",
+                verbose_name="Dead-line",
+            ),
         ),
     ]
