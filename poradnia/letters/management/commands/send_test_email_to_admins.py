2620cc9e76631130f4767a10f5b7b7b4695d880c
PiotrIw <piotr.iwanski@gmail.com>
2023-03-09 13:14:08 +0000

    chore: add email testing management command

---
+++ poradnia/letters/management/commands/send_test_email_to_admins.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = "Command that sends test email to app admins."
+
+    def handle(self, *args, **options):
+        send_mail(
+            subject="Test email to app admins",
+            message="Test message content.",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=settings.ADMINS,
+            fail_silently=False,
+        )
+        print(f"Test email to app admins sent over {settings.EMAIL_BACKEND}")
