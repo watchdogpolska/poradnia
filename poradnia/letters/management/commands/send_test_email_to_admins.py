0bf8a71b18918d145b57e7febf55fcb3302ee689
PiotrIw <piotr.iwanski@gmail.com>
2023-03-15 11:19:50 +0000

    chore: update send_test_email_to_admins

--- poradnia/letters/management/commands/send_test_email_to_admins.py
+++ poradnia/letters/management/commands/send_test_email_to_admins.py
@@ -7,11 +7,16 @@ class Command(BaseCommand):
     help = "Command that sends test email to app admins."
 
     def handle(self, *args, **options):
+        message_text = f"Hi \n\nThis is test message sent by {settings.APPS_DIR} app\n"
+        message_text += "\nFollowing local apps are registered:\n"
+        for app in settings.LOCAL_APPS:
+            message_text += f"  - {app}\n"
+        message_text += f"Test email to app admins sent using {settings.EMAIL_BACKEND}"         
         send_mail(
-            subject="Test email to app admins",
-            message="Test message content.",
+            subject="Test email from to app admins",
+            message=message_text,
             from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=settings.ADMINS,
+            recipient_list=[adm[1] for adm in settings.ADMINS],
             fail_silently=False,
         )
-        print(f"Test email to app admins sent over {settings.EMAIL_BACKEND}")
+        print(f"Test email to app admins sent using {settings.EMAIL_BACKEND}")
