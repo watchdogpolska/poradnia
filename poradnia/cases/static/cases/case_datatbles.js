d5036c6dafd295e65f4a0a9bed17b9f0c6ef3c7c
PiotrIw <piotr.iwanski@gmail.com>
2023-03-30 18:54:30 +0000

    feat: add datatable translation

--- poradnia/cases/static/cases/case_datatbles.js
+++ poradnia/cases/static/cases/case_datatbles.js
@@ -25,6 +25,28 @@
                     //     left: 1,
                     //     // right: 1
                     // }
+                    "language": {
+                        "processing":     "Przetwarzanie...",
+                        "search":         "Szukaj:",
+                        "lengthMenu":     "Pokaż _MENU_ pozycji",
+                        "info":           "Pozycje od _START_ do _END_ z _TOTAL_ łącznie",
+                        "infoEmpty":      "Pozycji 0 z 0 dostępnych",
+                        "infoFiltered":   "(filtrowanie spośród _MAX_ dostępnych pozycji)",
+                        "infoPostFix":    "",
+                        "loadingRecords": "Wczytywanie...",
+                        "zeroRecords":    "Nie znaleziono pasujących pozycji",
+                        "emptyTable":     "Brak danych",
+                        "paginate": {
+                            "first":      "Pierwsza",
+                            "previous":   "Poprzednia",
+                            "next":       "Następna",
+                            "last":       "Ostatnia"
+                        },
+                        "aria": {
+                            "sortAscending": ": aktywuj, by posortować kolumnę rosnąco",
+                            "sortDescending": ": aktywuj, by posortować kolumnę malejąco"
+                        }
+                    },
                 }, {
                     // extra_data
                     status_free: function() { return $("input[name='check_status_free']").is(":checked") ? 1 : 0; },
