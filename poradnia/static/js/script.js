61c15dac486b6745954b5fc10116b803064fa259
PiotrIw <piotr.iwanski@gmail.com>
2023-03-30 15:46:43 +0000

    chore update frontend dependencies

--- poradnia/static/js/script.js
+++ poradnia/static/js/script.js
@@ -63763,7 +63763,8 @@ window.AjaxDatatableViewUtils = (function() {
                 "/sprawy/case_table_ajax_data/",
                 {
                     // extra_options (example)
-                    processing: false,
+                    processing: true,
+                    serverSide: true,
                     autoWidth: true,
                     full_row_select: false,
                     scrollX: true,
@@ -63809,7 +63810,8 @@ window.AjaxDatatableViewUtils = (function() {
                 "/porady/advice_table_ajax_data/",
                 {
                     // extra_options (example)
-                    processing: false,
+                    processing: true,
+                    serverSide: true,
                     autoWidth: true,
                     full_row_select: false,
                     scrollX: true,
@@ -63836,37 +63838,3 @@ window.AjaxDatatableViewUtils = (function() {
         }
     });
 })(jQuery);
-// ;(function($) {
-//     $(document).ready(function () {
-//         const table1 = document.getElementById("datatable_advices");
-//         if (table1) {
-//             var tableTop = $("#tableWrapper")[0].getBoundingClientRect().top;
-//             var viewportHeight = $(window).innerHeight();
-//             var maxHeight = viewportHeight - tableTop;
-//             $("#tableWrapper").css({
-//                 maxHeight: maxHeight - 0,
-//             });
-//             AjaxDatatableViewUtils.initialize_table(
-//                 $('#datatable_advices'),
-//                 "/porady/advice_table_ajax_data/",
-//                 {
-//                     // extra_options (example)
-//                     processing: false,
-//                     autoWidth: true,
-//                     full_row_select: false,
-//                     scrollX: true,
-//                     // searching: false,
-//                     scrollY: maxHeight - 250,
-//                     // TODO make fixedColumns working !!!
-//                     // fixedColumns: {
-//                     //     left: 1,
-//                     //     // right: 1
-//                     // }
-//                 }, {
-//                     // extra_data
-//                     // ...
-//                 },
-//             );
-//         }
-//     });
-// })(jQuery);
