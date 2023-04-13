689805fd2c9968e6105fb414b4bb70530f2e4870
PiotrIw <piotr.iwanski@gmail.com>
2023-04-13 15:25:46 +0000

    feat: add event.completed property and it's handling

--- poradnia/events/management/commands/send_event_reminders.py
+++ poradnia/events/management/commands/send_event_reminders.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import logging
 
 from django.core.management import BaseCommand
 from django.db.models import Q
@@ -7,6 +8,8 @@ from django.utils import timezone
 from poradnia.cases.utils import get_users_with_perm
 from poradnia.events.models import Event, Reminder
 from poradnia.users.models import User
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -50,9 +53,9 @@ class Command(BaseCommand):
 
         notification_deadline = timedelta(days=deadline_days)
 
-        if self.today + notification_deadline > event.time:
+        if (self.today + notification_deadline > event.time) and (not event.completed):
             msg = "Sending notification about {} to user {}".format(event, user)
-            self.stdout.write(msg)
+            logger.info(msg)
 
             user.notify(
                 actor=user,
@@ -61,3 +64,6 @@ class Command(BaseCommand):
                 from_email=event.case.get_email(),
             )
             Reminder.objects.create(event=event, user=user)
+        elif (self.today + notification_deadline > event.time) and event.completed:
+            msg = "Event {} is completed, skipping notification".format(event)
+            logger.info(msg)
