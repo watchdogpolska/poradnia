428273a3a8de44e3cdea21fa71e6dbccb928c2d3
PiotrIw <piotr.iwanski@gmail.com>
2025-09-29 10:35:09 +0000

    fix linters

--- poradnia/utils/crispy_forms.py
+++ poradnia/utils/crispy_forms.py
@@ -2,6 +2,7 @@
 Local crispy forms helpers to replace the unmaintained atom library.
 This provides compatible replacements for the atom.ext.crispy_forms functionality.
 """
+
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.contrib import messages
@@ -13,8 +14,9 @@ from django.utils.translation import gettext as _
 
 class FormsetHelper(FormHelper):
     """Base formset helper with common settings."""
+
     form_tag = False
-    form_method = 'post'
+    form_method = "post"
 
 
 class TableFormSetHelper(FormsetHelper):
@@ -22,13 +24,15 @@ class TableFormSetHelper(FormsetHelper):
     Formset helper for table-based inline formsets.
     Uses bootstrap3 compatible template instead of the missing bootstrap template.
     """
-    # Use the built-in bootstrap3 template instead of the missing 
+
+    # Use the built-in bootstrap3 template instead of the missing
     # bootstrap/table_inline_formset.html
-    template = 'bootstrap3/table_inline_formset.html'
+    template = "bootstrap3/table_inline_formset.html"
 
 
 class TableFormSetMixin:
     """Mixin to add table formset helper to formsets."""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = TableFormSetHelper()
@@ -36,11 +40,13 @@ class TableFormSetMixin:
 
 class InlineTableFormSet(TableFormSetMixin, BaseInlineFormSet):
     """Inline formset with table helper."""
+
     pass
 
 
 class TableFormSet(TableFormSetMixin, BaseFormSet):
     """Regular formset with table helper."""
+
     pass
 
 
@@ -48,6 +54,7 @@ class BaseTableFormSet(BaseInlineFormSet):
     """
     Base table formset compatible with the atom library version.
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = TableFormSetHelper()
@@ -55,17 +62,18 @@ class BaseTableFormSet(BaseInlineFormSet):
 
 class HelperMixin:
     """Mixin to add form helper to forms."""
+
     form_helper_cls = FormHelper
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = getattr(self, 'helper', self.form_helper_cls(self))
+        self.helper = getattr(self, "helper", self.form_helper_cls(self))
 
 
 class SingleButtonMixin(HelperMixin):
     """
     Dynamically add crispy button to form layout.
-    
+
     Example:
         class PersonModelForm(SingleButtonMixin, forms.ModelForm):
             class Meta:
@@ -75,24 +83,29 @@ class SingleButtonMixin(HelperMixin):
     @property
     def action_text(self):
         """Text for the action submit button."""
-        return _('Update') if hasattr(self, 'instance') and self.instance.pk else _('Save')
+        return (
+            _("Update") if hasattr(self, "instance") and self.instance.pk else _("Save")
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper.add_input(Submit('action', self.action_text, css_class="btn-primary"))
+        self.helper.add_input(
+            Submit("action", self.action_text, css_class="btn-primary")
+        )
 
 
 class FormHorizontalMixin(HelperMixin):
     """Mixin to make forms horizontal with Bootstrap classes."""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-lg-3'
-        self.helper.field_class = 'col-lg-9'
+        self.helper.form_class = "form-horizontal"
+        self.helper.label_class = "col-lg-3"
+        self.helper.field_class = "col-lg-9"
 
 
 # View mixins
-class FormSetMixin(object):
+class FormSetMixin:
     inline_model = None
     inline_form_cls = None
     formset_cls = BaseTableFormSet
@@ -105,21 +118,24 @@ class FormSetMixin(object):
         return self.object.get_absolute_url()
 
     def get_formset(self):
-        return self.formset or inlineformset_factory(self.model, self.inline_model,
-                                                     form=self.inline_form_cls,
-                                                     formset=self.formset_cls)
+        return self.formset or inlineformset_factory(
+            self.model,
+            self.inline_model,
+            form=self.inline_form_cls,
+            formset=self.formset_cls,
+        )
 
     def get_context_data(self, **kwargs):
-        context = super(FormSetMixin, self).get_context_data(**kwargs)
-        context.update({'formset': self.get_formset()(instance=self.get_instance())})
+        context = super().get_context_data(**kwargs)
+        context.update({"formset": self.get_formset()(instance=self.get_instance())})
         return context
 
     def get_formset_valid_message(self):
         return _("{0} created!").format(self.object)
 
     def get_form(self, *args, **kwargs):
-        form = super(FormSetMixin, self).get_form(*args, **kwargs)
-        if hasattr(form, 'helper'):
+        form = super().get_form(*args, **kwargs)
+        if hasattr(form, "helper"):
             form.helper.form_tag = False
         return form
 
@@ -129,7 +145,9 @@ class FormSetMixin(object):
         return HttpResponseRedirect(self.get_success_url())
 
     def formset_invalid(self, form, formset):
-        return self.render_to_response(self.get_context_data(form=form, formset=formset))
+        return self.render_to_response(
+            self.get_context_data(form=form, formset=formset)
+        )
 
     def get_formset_kwargs(self):
         return {}
@@ -137,10 +155,12 @@ class FormSetMixin(object):
     def form_valid(self, form):
         self.object = form.save(commit=False)
         FormSet = self.get_formset()
-        formset = FormSet(self.request.POST or None,
-                          self.request.FILES,
-                          instance=self.object,
-                          **self.get_formset_kwargs())
+        formset = FormSet(
+            self.request.POST or None,
+            self.request.FILES,
+            instance=self.object,
+            **self.get_formset_kwargs(),
+        )
 
         if formset.is_valid():
             self.object.save()
