9d2d93b11146f645dbc093b2347f59ec0f858e36
PiotrIw <piotr.iwanski@gmail.com>
2025-09-29 09:04:58 +0000

    fix liners

--- poradnia/users/login_form.py
+++ poradnia/users/login_form.py
@@ -1,7 +1,7 @@
 from allauth.account.forms import LoginForm
 from atom.ext.crispy_forms.forms import SingleButtonMixin
 from crispy_forms.bootstrap import PrependedText
-from crispy_forms.layout import Field, Layout
+from crispy_forms.layout import Layout
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy as _l
