8901784709bae75174430ade03b0d42a4712a25a
PiotrIw <piotr.iwanski@gmail.com>
2026-04-23 12:39:26 +0000

    allow delete advice in admin

--- poradnia/advicer/admin.py
+++ poradnia/advicer/admin.py
@@ -57,9 +57,6 @@ class AdviceAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     def has_change_permission(self, request, obj=None):
         return False
 
