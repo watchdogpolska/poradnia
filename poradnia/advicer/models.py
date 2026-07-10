f04ce2974b22a3c67bfa235a0e7994d562cabe0c
PiotrIw <piotr.iwanski@gmail.com>
2026-07-10 12:14:43 +0200

    code linting and translations update

--- poradnia/advicer/models.py
+++ poradnia/advicer/models.py
@@ -291,7 +291,9 @@ class Advice(models.Model):
         pk = self._ai_tag("institution_kind")
         if pk is None:
             return None
-        return InstitutionKind.objects.filter(pk=pk).values_list("name", flat=True).first()
+        return (
+            InstitutionKind.objects.filter(pk=pk).values_list("name", flat=True).first()
+        )
 
     @property
     def ai_jst(self):
