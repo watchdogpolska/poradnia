234b2ca2ba7155abe8f3e5b4a8b76a02809c191f
PiotrIw <piotr.iwanski@gmail.com>
2026-09-02 17:23:04 +0000

    make Event admin usable

--- poradnia/events/admin.py
+++ poradnia/events/admin.py
@@ -5,8 +5,30 @@ from .models import Event, Reminder
 
 class ReminderInline(admin.StackedInline):
     model = Reminder
+    readonly_fields = ["user"]
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    date_hierarchy = "time"
     inlines = [ReminderInline]
+    list_display = [
+        "id",
+        "case__id",
+        "deadline",
+        "completed",
+        "public",
+        "time",
+    ]
+    list_filter = ["completed"]
+    search_fields = ["id", "name", "location"]
+    ordering = ["-id"]
+    actions = None
+    readonly_fields = [
+        "id",
+        "case",
+        "created_by",
+        "created_on",
+        "modified_by",
+        "modified_on",
+    ]
